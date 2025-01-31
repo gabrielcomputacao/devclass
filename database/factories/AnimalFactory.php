68b2bf46a779320e3e1375b0098f345981bc196b
Gabriel <gabrielcomputacao@outlook.com>
2025-01-31 08:13:27 -0300

    feat: practicing database concepts

---
+++ database/factories/AnimalFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class AnimalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'raca' => fake()->name(),
+            'cor' => fake()->lastName(),
+            'tamanho' => fake()->numberBetween(1, 100),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\TesteCreateController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,5 @@ Route::get('/teste-create', function () {
 
 
 Route::post('/test-post-create', [TesteCreateController::class, 'store']);
+
+Route::get('/cart', [CartController::class, 'index']);
